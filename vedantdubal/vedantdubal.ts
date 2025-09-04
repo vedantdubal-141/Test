433154341ebf2d3e199a4b3c79a66ab96eddf51d
vedantdubal-141 <vedant.dubal.cg@gmail.com>
2025-09-04 01:53:54 -0400

    vedantdubal

---
+++ vedantdubal/vedantdubal.ts
@@ -0,0 +1,18 @@
+let _id; // Implicit Declearation
+let fb_id: any; // Explicit Declearation
+fb_id = true;
+fb_id = 23423523;
+
+const firstName = "Riyad";
+const lastName: string = "Hossain";
+let isLoading: boolean;
+isLoading = false;
+
+let roll: string | number = 423;
+roll = "423";
+
+let device: string | number | boolean; // Union Type
+device = "Mobile";
+device = 4323523;
+
+const re: RegExp = /\w+/g;
